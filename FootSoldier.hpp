f4f46e550afca77193f8707eaca09dbb9dd6f4a8
ohadcohen1111 <ohadcohen1111@gmail.com>
2020-05-20 16:31:49 +0300

    #First Update

---
+++ FootSoldier.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class FootSoldier
+{
+private:
+    int health;
+    int damage;
+    int player;
+
+public:
+    FootSoldier(int player){this.player = player;}
+    ~FootSoldier();
+};
+
+
